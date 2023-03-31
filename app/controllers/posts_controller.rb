7e516b38098725a56f4c1d371444d5a27b0632eb
user4ge <masumi.kurashige@gmail.com>
2023-04-01 06:40:59 +0900

    メモを降順(新しい順)に表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: "DESC")
   end
 
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
-  end
 end
